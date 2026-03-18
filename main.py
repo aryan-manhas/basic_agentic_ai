73351841177db3ec4850caedd2b9434fa2bf7973
Aryan <aryanmanhas06@gmail.com>
2026-03-18 11:22:06 +0000

    Update main.py: Saves the memory of user prompt for better response

--- main.py
+++ main.py
@@ -2,23 +2,30 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
 
 
 load_dotenv()
 
 
 def main():
+
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("API Key Not Provided!")
     client = genai.Client(api_key=api_key)
+
     parser = argparse.ArgumentParser(description="Agentic AI Bot")
     parser.add_argument("user_prompt", type=str, help="Prompt")
     args = parser.parse_args()
+
+    message = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.user_prompt,
+        contents=message,
     )
+
     tokens = response.usage_metadata
     if tokens is None:
         RuntimeError("API Request Failed!")
