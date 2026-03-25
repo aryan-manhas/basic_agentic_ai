85726953a0664891c445fc104c5f9e363ebfe094
Aryan <aryanmanhas06@gmail.com>
2026-03-25 02:12:20 +0000

    Testing get_files_info() function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -10,7 +10,34 @@ def get_files_info(working_directory, directory="."):
         == working_directory_path
     )
 
+    if directory == ".":
+        print("Result for current directory:")
+    else:
+        print(f"Result for {directory} directory:")
+
     if valid_target_dir is False:
         print(
             f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         )
+        return
+
+    if not os.path.isdir(target_dir):
+        print(f'Error: "{directory}" is not a directory')
+        return
+
+    info_list = []
+
+    for item in os.listdir(target_dir):
+        item_path = os.path.join(target_dir, item)
+
+        is_dir = os.path.isdir(item_path)
+        file_size = os.path.getsize(item_path)
+
+        line = f"- {item}: file_size={file_size}, is_dir={is_dir}"
+        info_list.append(line)
+
+    if info_list is []:
+        print(f"Error: {directory} is empty.")
+    else:
+        for line in info_list:
+            print(line)
