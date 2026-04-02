2ee904676be1cf13f158776c1397d9571d592c59
Aryan <aryanmanhas06@gmail.com>
2026-04-02 03:01:47 +0000

    Updated get_files_content.py

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -10,19 +10,21 @@ def get_file_content(working_directory, file_path):
         == working_doirectory_path
     )
 
-    if valid_target != False:
+    if not valid_target:
         print(
             f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
         )
         return
 
-    if not os.path.isfile(valid_target):
+    if not os.path.isfile(target_path):
         print(f'Error: File not found or is not a regular file: "{file_path}')
+        return
 
     MAX_CHAR = 1000
 
-    with open(file_path, "r") as f:
-        file_string = f.read(MAX_CHAR)
+    with open(target_path, "r") as f:
+        content = f.read(MAX_CHAR)
 
         if f.read(1):
             content += f'[...File "{file_path}" truncated at {MAX_CHAR} characters]'
+            print(content)
---
+++ test_get_file_content.py
@@ -0,0 +1,12 @@
+from functions.get_files_content import get_file_content
+
+
+def test():
+    get_file_content(".", "main.py")
+    get_file_content("calculator", "pkg/calculator.py")
+    get_file_content("calculator", "/bin/cat")
+    get_file_content("calculator", "pkg/does_not_exist.py")
+
+
+if __name__ == "__main__":
+    test()
