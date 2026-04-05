4a64682b724fd66cb8b5bfd754c7fe05c57568dc
Aryan <aryanmanhas06@gmail.com>
2026-04-05 15:24:15 +0000

    Added write_file.py

---
+++ functions/write_file.py
@@ -0,0 +1,37 @@
+import os
+
+
+def write_file(working_directory, file_path, content):
+    try:
+        # Checks the path of the working directory and creates a absolute path for the target directory
+        working_doirectory_path = os.path.abspath(working_directory)
+        target_path = os.path.normpath(os.path.join(working_doirectory_path, file_path))
+
+        # Makes sure target directory is the part of working directory
+        valid_target = (
+            os.path.commonpath([working_doirectory_path, target_path])
+            == working_doirectory_path
+        )
+
+        # Makes sure target file is not out of bounds
+        if not valid_target:
+            print(
+                f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+            )
+            return
+
+        # Makes sure the target file is actually a file and not a directory
+        if os.path.isdir(target_path):
+            print(f'Error: Cannot write to "{file_path}" as it is a directory')
+            return
+
+        os.makedirs(os.path.dirname(target_path), exist_ok=True)
+
+        with open(target_path, "w") as f:
+            f.write(content)
+            print(
+                f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+            )
+
+    except:
+        print("Error: Standard Library Function Error.")
---
+++ test_write_file.py
@@ -0,0 +1,11 @@
+from functions.write_file import write_file
+
+
+def test():
+    write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+
+
+if __name__ == "__main__":
+    test()
